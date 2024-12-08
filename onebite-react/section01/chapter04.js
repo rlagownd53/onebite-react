44cc67b85ae871ff2fe543cb15d9e80a94b6ac26
zumsim <rlagownd53@naver.com>
2024-12-08 20:14:38 +0900

    241208

---
+++ onebite-react/section01/chapter04.js
@@ -0,0 +1,23 @@
+// 1. 변수
+let age = 27;
+
+age = 30;
+
+// 2. 상수
+const birth = "1997.01.07";
+
+// 3. 변수 명명규칙(네이밍 규칙)
+// 3-1. $, _ 를 제외한 기호는 사용한 수 없다.
+let $_name;
+
+// 3-2. 숫자로 시작할 수 없다.
+let name1;
+let _2name;
+
+// 3-3. 예약어를 사용할 수 없다.
+
+// 4. 변수 명명 가이드
+// a,b,c 이런식이 아닌 사용에 맞는 네이밍을 해야 편리하다.
+let salesCount = 1;
+let refundCount = 1;
+let totalSalesCount = salesCount - refundCount;
