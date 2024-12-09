9b1d4daa2db546f72929b003809a2ac42c5e2c49
zumsim <rlagownd53@naver.com>
2024-12-09 21:16:33 +0900

    241209

---
+++ onebite-react/section01/chapter05.js
@@ -0,0 +1,27 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nam = NaN;
+
+// 2. String "Type
+let myName = "김해중";
+let myLocation = "김포";
+let introduce = myName + myLocation;
+
+// 템플릿 리터럴 문법
+let introduceText = `${myName}은 ${myLocation}에 거주합니다.`;
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다)
+let empty = null;
+
+// 5. Undefined Type
+let none;
