0884a1d3cfea43ae86a63a4b5e40446149cd65a7
zumsim <rlagownd113@naver.com>
2025-03-15 21:40:07 +0900

    250315

---
+++ onebite-react/section03/src/index.js
@@ -0,0 +1,10 @@
+import mul, { add, sub } from "./math.js";
+
+//라이브러리를 불러올 땐 뒤에 경로가 아니라 그냥 이름만 명시
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+//console.log(add(1, 2));
+//console.log(sub(1, 2));
+//console.log(mul(2, 3));
