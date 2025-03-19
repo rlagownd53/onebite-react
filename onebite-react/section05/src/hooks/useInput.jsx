99890bcac654fd6a4ab9d1f7588f048730d6f88f
zumsim <rlagownd113@naver.com>
2025-03-19 20:03:11 +0900

    250319

---
+++ onebite-react/section05/src/hooks/useInput.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function useInput() {
+  const [input, setInput] = useState("");
+
+  const onChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  return [input, onchange];
+}
+
+export default useInput;
