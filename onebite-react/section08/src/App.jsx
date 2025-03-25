a6d915ab2e695559cfab15ce3f6b2bc04bbd1ec1
zumsim <rlagownd113@naver.com>
2025-03-25 20:45:15 +0900

    250325

--- onebite-react/section08/src/App.jsx
+++ onebite-react/section08/src/App.jsx
@@ -1,7 +1,52 @@
 import "./App.css";
+import { useState, useRef } from "react";
+import Header from "./components/Header";
+import List from "./components/List";
+import Editor from "./components/Editor";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "React 공부하기2",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "React 공부하기3",
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
-  return <>TodoList</>;
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <Editor onCreate={onCreate} />
+      <List />
+    </div>
+  );
 }
 
 export default App;
